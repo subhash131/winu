99e258ea1a340939feab9ab8f975e327d7b8fa19
subhash131 <subhashnayak131@gmail.com>
2024-09-17 00:59:40 +0530

    create component: TsParticles

---
+++ client/src/components/particles.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+
+export const TsParticles = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <>
+      {init && (
+        <Particles
+          id="tsparticles"
+          options={{
+            background: {
+              color: {
+                value: "transparent",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true as any,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 100,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#333233",
+              },
+              links: {
+                enable: false,
+                color: {
+                  value: "#333233",
+                },
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 1,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  height: 800,
+                  width: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 1,
+              },
+              shape: {
+                type: ["star"],
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      )}
+    </>
+  );
+};
