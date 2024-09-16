4150d656bde9d1c2891cb53442f8d907c6b0ae58
subhash131 <subhashnayak131@gmail.com>
2024-09-17 00:59:53 +0530

    create component: WalletButton

---
+++ client/src/components/wallet-button.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import React, { useEffect, useState } from "react";
+
+const WalletButton = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <WalletMultiButton
+      style={{
+        backgroundColor: "#000",
+      }}
+    />
+  );
+};
+
+export default WalletButton;
