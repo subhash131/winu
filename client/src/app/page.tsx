99bae1d285bf630d44341152823b6df916da06be
subhash131 <subhashnayak131@gmail.com>
2024-09-17 00:58:49 +0530

    update landing page css

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import GradientOverlay from "@/components/gradient-overlay";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className=" w-screen h-screen overflow-hidden text-white relative">
+      <GradientOverlay />
+      <div className="size-full absolute top-0 left-0 z-10">
+        <Navbar />
+      </div>
+    </main>
+  );
 }
