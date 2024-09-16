bd33600f7aea11a7042b7b958faa62ef9f830c4c
subhash131 <subhashnayak131@gmail.com>
2024-09-17 01:00:06 +0530

    setup redux

---
+++ client/src/providers/app-provider.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { store } from "@/state-manager/store";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import React, { useMemo } from "react";
+import { Provider } from "react-redux";
+
+const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  const network = "devnet";
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <Provider store={store}>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </Provider>
+  );
+};
+
+export default AppProvider;
---
+++ client/src/state-manager/store.ts
@@ -0,0 +1,10 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const reducer = combineReducers({});
+
+export const store = configureStore({
+  reducer,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
