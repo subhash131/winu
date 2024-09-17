4296449efbafb94cf876afac0adb681c973d1df5
subhash131 <subhashnayak131@gmail.com>
2024-09-17 09:35:43 +0530

    update CreateVenue: add overlay

--- client/src/components/create-venue/index.tsx
+++ client/src/components/create-venue/index.tsx
@@ -4,7 +4,8 @@ import Header from "./header";
 const CreateVenue = () => {
   return (
     <main className=" w-screen h-screen overflow-hidden text-white relative">
-      <div className="size-full top-0 left-0 z-10 pt-32 px-80 max-md:px-20">
+      <Overlay />
+      <div className="size-full top-0 left-0 z-10 pt-32 px-72 max-lg:px-20 ">
         <Header />
         <div className="h-full w-full"></div>
       </div>
@@ -13,3 +14,19 @@ const CreateVenue = () => {
 };
 
 export default CreateVenue;
+
+const Overlay = () => {
+  return (
+    <div className="absolute size-full">
+      <div
+        className={`bg-gradient-glow size-96 blur-2xl after:absolute after:w-[90%] after:h-[80%] after:top-[25%] after:left-0 after:bg-gradient-glow after:blur-2xl absolute left-[40%] rounded-b-full after:rounded-b-full top-64`}
+      ></div>
+      <div
+        className={`bg-gradient-glow size-40 blur-2xl after:absolute after:w-[90%] after:h-[80%] after:top-[25%] after:left-0 after:bg-gradient-glow after:blur-2xl absolute left-[2%] rounded-b-full top-40 after:rounded-b-full`}
+      ></div>
+      <div
+        className={`bg-gradient-glow size-40 blur-2xl after:absolute after:w-[90%] after:h-[80%] after:top-[25%] after:left-0 after:bg-gradient-glow after:blur-2xl absolute left-[2%] rounded-b-full top-[50%] bottom-[50%] after:rounded-b-full`}
+      ></div>
+    </div>
+  );
+};
