1f97220c289592ae78b97ba50337af83b0fead2c
subhash131 <subhashnayak131@gmail.com>
2024-09-17 12:57:21 +0530

    create component: VenueList

--- client/src/components/venue-list.tsx
+++ client/src/components/venue-list.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import VenueCard from "./venue-card";
 
 const VenueList = () => {
-  return <div>VenueList</div>;
+  return (
+    <div className="h-full w-full overflow-scroll pb-32">
+      <VenueCard />
+      <VenueCard />
+      <VenueCard />
+      <VenueCard />
+    </div>
+  );
 };
 
 export default VenueList;
