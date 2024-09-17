be96e4dd09329f973912a76ae17c1b5433b43408
subhash131 <subhashnayak131@gmail.com>
2024-09-17 18:41:18 +0530

    create component: AddTeams

---
+++ client/src/components/add-teams.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddTeams = () => {
+  return (
+    <div className="pt-10 size-full">
+      <h3 className="text-2xl">Add teams</h3>
+      <div className="w-full h-fit"></div>
+    </div>
+  );
+};
+
+export default AddTeams;
