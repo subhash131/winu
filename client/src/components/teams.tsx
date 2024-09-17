4cfe687b8d9b6033c86eb341d1eda823165dbfc1
subhash131 <subhashnayak131@gmail.com>
2024-09-18 00:53:20 +0530

    Update CSS:Teams

--- client/src/components/teams.tsx
+++ client/src/components/teams.tsx
@@ -3,7 +3,7 @@ import TeamCard from "./team-card";
 
 const Teams = () => {
   return (
-    <div className="pt-10 h-fit w-full">
+    <div className="pt-4 h-fit w-full">
       <h3 className="text-2xl pb-6 ">Teams</h3>
       <div className="w-full h-fit grid [grid-template-columns:repeat(auto-fit,minmax(300px,1fr))] gap-4">
         <TeamCard />
