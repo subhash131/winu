803463dd701354313817bc3af47f8dda3523f8a8
subhash131 <subhashnayak131@gmail.com>
2024-09-17 09:45:38 +0530

    update css: venue card

--- client/src/components/create-venue/index.tsx
+++ client/src/components/create-venue/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./header";
+import VenueCard from "./venue-card";
 
 const CreateVenue = () => {
   return (
@@ -7,7 +8,9 @@ const CreateVenue = () => {
       <Overlay />
       <div className="size-full top-0 left-0 z-10 pt-32 px-72 max-lg:px-20 ">
         <Header />
-        <div className="h-full w-full"></div>
+        <div className="h-full w-full">
+          <VenueCard />
+        </div>
       </div>
     </main>
   );
@@ -17,7 +20,7 @@ export default CreateVenue;
 
 const Overlay = () => {
   return (
-    <div className="absolute size-full">
+    <div className="absolute size-full -z-10">
       <div
         className={`bg-gradient-glow size-96 blur-2xl after:absolute after:w-[90%] after:h-[80%] after:top-[25%] after:left-0 after:bg-gradient-glow after:blur-2xl absolute left-[40%] rounded-b-full after:rounded-b-full top-64`}
       ></div>
--- client/src/components/create-venue/venue-card.tsx
+++ client/src/components/create-venue/venue-card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const VenueCard = () => {
-  return <div>VenueCard</div>;
+  return <div className="w-full h-60 border "></div>;
 };
 
 export default VenueCard;
