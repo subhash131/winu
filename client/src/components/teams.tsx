b1ee48d853266c69d65c182c68bf683b0058b152
subhash131 <subhashnayak131@gmail.com>
2024-09-17 22:16:36 +0530

    create component:Teams

---
+++ client/src/components/teams.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TeamCard from "./team-card";
+
+const Teams = () => {
+  return (
+    <div className="pt-10 h-fit w-full">
+      <h3 className="text-2xl pb-6 ">Teams</h3>
+      <div className="w-full h-fit grid [grid-template-columns:repeat(auto-fit,minmax(300px,1fr))] gap-4">
+        <TeamCard />
+        <TeamCard />
+        <TeamCard />
+        <TeamCard />
+      </div>
+    </div>
+  );
+};
+
+export default Teams;
