e668231642a9e006df9574887f116376a4072055
subhash131 <subhashnayak131@gmail.com>
2024-09-17 09:24:51 +0530

    add component: create venue

---
+++ client/src/components/create-venue/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./header";
+
+const CreateVenue = () => {
+  return (
+    <main className=" w-screen h-screen overflow-hidden text-white relative">
+      <div className="size-full top-0 left-0 z-10 pt-32 px-80 max-md:px-20">
+        <Header />
+        <div className="h-full w-full"></div>
+      </div>
+    </main>
+  );
+};
+
+export default CreateVenue;
