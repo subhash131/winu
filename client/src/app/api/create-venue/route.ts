545c4777df3c5b46a58b54d4c4d89aeed737a858
subhash131 <subhashnayak131@gmail.com>
2024-09-18 22:25:49 +0530

    create route: create-venue

---
+++ client/src/app/api/create-venue/route.ts
@@ -0,0 +1,29 @@
+"use server";
+import { connect } from "@/db/index";
+import Venue from "@/models/venue";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  await connect();
+  try {
+    const reqBody = await req.json();
+    console.log("🚀 ~ POST ~ reqBody:", reqBody);
+    const { name, startDate, endDate, imageUrl, streamLink, description } =
+      reqBody;
+    console.log("🚀 ~ POST ~ startDate:", startDate);
+
+    const newVenue = new Venue({
+      name,
+      startDate: new Date(),
+      endDate: new Date(),
+      imageUrl,
+      streamLink,
+      description,
+    });
+
+    const res = await newVenue.save();
+    return NextResponse.json({ ...res._doc }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 });
+  }
+}
