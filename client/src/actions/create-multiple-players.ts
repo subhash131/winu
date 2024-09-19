fecdb5efa177309bb5bc5b2b7c1378681dae08e6
subhash131 <subhashnayak131@gmail.com>
2024-09-20 02:37:12 +0530

    create route: bulk player create

---
+++ client/src/actions/create-multiple-players.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { TPlayer } from "@/types/player";
+
+export async function createVenue({ players }: { players: TPlayer[] }) {
+  try {
+    const res = await fetch("http://localhost:3000/api/bulk/player", {
+      method: "POST",
+      body: JSON.stringify(players),
+      cache: "no-store",
+    });
+    const newPlayers = await res.json();
+    return newPlayers;
+  } catch (err) {
+    console.log("Failed to create venue", err);
+  }
+}
--- client/src/app/api/bulk/player/route.ts
+++ client/src/app/api/bulk/player/route.ts
@@ -1,6 +1,6 @@
 "use server";
 import { connect } from "@/db/index";
-import player from "@/models/player";
+import Player from "@/models/player";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -8,15 +8,10 @@ export async function POST(req: NextRequest) {
   try {
     const reqBody = await req.json();
     console.log("🚀 ~ POST ~ reqBody:", reqBody);
-    const { description, username, imageUrl } = reqBody;
 
-    const newPlayer = new player({
-      description,
-      username,
-      imageUrl,
-    });
-    const res = await newPlayer.save();
-    return NextResponse.json({ ...res._doc }, { status: 201 });
+    const res = await Player.insertMany(reqBody);
+
+    return NextResponse.json(res, { status: 201 });
   } catch (err) {
     return NextResponse.json({ error: err }, { status: 500 });
   }
