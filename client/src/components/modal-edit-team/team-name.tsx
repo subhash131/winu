2eaab674bd9175144db9d001b258fdcfc0ee90a2
subhash131 <subhashnayak131@gmail.com>
2024-09-19 21:24:51 +0530

    update teamname

--- client/src/components/modal-edit-team/team-name.tsx
+++ client/src/components/modal-edit-team/team-name.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+"use client";
+import { updateTeamName } from "@/state-manager/features/team-form";
+import { RootState } from "@/state-manager/store";
+import React, { ChangeEvent } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const TeamName = () => {
+  const { name } = useSelector((state: RootState) => state.TeamForm);
+  const dispatch = useDispatch();
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(updateTeamName(e.target.value));
+  };
   return (
     <input
       className="text-xl font-semibold bg-transparent outline-none"
       placeholder="Team name"
+      name={name}
+      onChange={handleNameChange}
     />
   );
 };
